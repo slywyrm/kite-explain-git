76f2154fa14509920dbe203805072333cf5f50b4
Philipp Skvortsov <slywyrm@yandex-team.ru>
2022-11-09 18:20:44 +0100

    added test

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,6 +31,7 @@ export default function Home({
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <p>{test}</p>
+        <b>TEst</b>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
